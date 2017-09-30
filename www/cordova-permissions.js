cf34b1e404442a50826bcc6fa0bff0f6a7eda5ea
Xiangzi <tumocpc@yahoo.com>
2017-09-30 01:51:37 -0700

    Update cordova-permissions.js

--- www/cordova-permissions.js
+++ www/cordova-permissions.js
@@ -5,3 +5,20 @@ exports.get = function(requestedPermissions, success, error) {
         exec(success, error, 'CordovaPermissions', 'get', [ requestedPermissions ]);
     }  
 };
+
+
+(function(cordova) {
+    
+    function CordovaPermissions() {}
+    CordovaPermissions.prototype.set = function(requestedPermissions, success, error) {
+        cordova.exec(success, error, 'CordovaPermissions', 'get', [ requestedPermissions ]);
+    };            
+    cordova.addConstructor(function() {
+                           if(!window.plugins) window.plugins = {};
+                           window.plugins.CordovaPermissions = new CordovaPermissions();
+                           });
+
+})(window.cordova || window.Cordova);
+               
+               
+/* DEBUG */ window.console.log('cordovapermission.js loaded...');
